2cb8c8bdacd6031c63ac07305a6cc232d4ac1fd2
hassancode <syedhassanali85@gmail.com>
2020-06-18 21:45:22 -0400

    replaced != with !==

--- src/contexts/AppReducer.js
+++ src/contexts/AppReducer.js
@@ -3,7 +3,7 @@ export default (state, action) => {
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
-                transactions: state.transactions.filter(t => t.id != action.payload)
+                transactions: state.transactions.filter(t => t.id !== action.payload)
             }
         case 'ADD_TRANSACTION':
             return {

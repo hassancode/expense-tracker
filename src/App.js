2ca1ab18e08acc50d310328eaaa75b650e8194d2
hassancode <syedhassanali85@gmail.com>
2020-06-18 21:39:54 -0400

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
-// import Cards from './components/Cards/Cards';
-// import Transaction from './components/Transaction/Transaction'
-// import ExpenseTable from './components/ExpenseTable/ExpenseTable';
+import React from 'react';
 import { Header } from './components/Header/Header';
 import { Balance } from './components/Balance/Balance';
 import { AccountSummary } from './components/AccountSummary/AccountSummary';
 import AddTransaction from './components/AddTransaction/AddTransaction';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import {TransactionList} from './components/TransactionList/TransactionList';
-import { TransactionProvider } from './contexts/TransactionContext'; //'./providers/TransactionProvider';
+import { TransactionProvider } from './contexts/TransactionContext'; 
 
 const App = () => {
-    const [income, setIncome] = useState(1000.0);
-    const [expenses, setExpenses] = useState(0.0);
-
-    function addTransaction(value) {
-        console.log(value);
-    }
-
     return (
         <TransactionProvider>
             <Header />

a1a4bc15dfda00b5029170c3fd5e33a561e83421
hassancode <syedhassanali85@gmail.com>
2020-06-27 17:04:19 -0400

    fixed income styling

--- src/components/AccountSummary/AccountSummary.js
+++ src/components/AccountSummary/AccountSummary.js
@@ -32,7 +32,7 @@ export const AccountSummary = () => {
   const amounts = transactions.map(t => t.amount);
   const incomes = amounts.filter(a => a > 0);
   const expenses = amounts.filter(a => a < 0);
-  const totalIncome = incomes.reduce((total, income) => (total += income), 0);
+  const totalIncome = incomes.reduce((total, income) => (total += income), 0).toLocaleString('en', {maximumFractionDigits:2});
   const totalExpenses = (expenses.reduce((total, expense) => (total += expense), 0) * -1).toLocaleString('en', {maximumFractionDigits:2});
   return (
     <Grid container spacing={2} justify="center">
